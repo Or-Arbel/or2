dfe9f9700a383b592c1c89e6aa20780de9bc88e1
Or-Arbel <orarbel1995@gmail.com>
2020-02-16 19:20:12 +0200

    test

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {HashRouter as Router,Swotch,Route,Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <Link to='/'>HomePage</Link>
+      <Link to='/page2'>2</Link>
+
+        <Switch>
+
+        <Route exact path='/' component={()=>{return <h1>Home Page</h1>}} />
+        <Route exact path='/page2' component={()=>{return <h1>Page 2</h1>}} />
+
+        </Switch>
+      </Router>
+      
+      
+    </div>
+  );
+}
+
+export default App;
